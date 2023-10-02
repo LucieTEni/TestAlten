97495699efdc38de073e0a7a148edb4091dec00a
LucieTEni <lucie.touchard2019@campus-eni.fr>
2023-10-02 16:52:57 +0200

    create public list
    create public list

--- front/src/app/product/products/products.component.ts
+++ front/src/app/product/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core'
 import { ProductsService } from '../products.service';
 import { Product, STOCK } from '../product.model';
 import { SearchParams } from 'app/shared/ui/list/search.model';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-products',
@@ -14,7 +15,14 @@ export class ProductsComponent implements OnInit {
   @ViewChild('templateListProduct', {static: true}) templateListProduct: TemplateRef<any>;
   @Input() public lang = 'en';
   public data: Product[] = [];
-  public sortKey = STOCK.INSTOCK;
+  public sortKeyTitle = "name-stock-rating-price";
+  public listKey = "sortBy";
+  public sortOptions:SelectItem[] = [
+    {label: 'Name', value: 'name'},
+    {label: 'Price', value: 'price'},
+    {label: 'Stock', value: 'inventoryStatus'},
+    {label: 'Rating', value: 'rating'}
+  ];
 
   constructor(
     private productsService: ProductsService
@@ -25,6 +33,24 @@ export class ProductsComponent implements OnInit {
   }
 
   getElements(event: SearchParams): void {
-    console.log(event);
+    this.data = this.productsService.getProducts();
+    if (event.search === '') {
+      return;
+    }
+
+    var search: string = event.search.trim().replace(/\s/g, '').toUpperCase();
+    var sortOrder = event.sortOrder;
+
+    this.data = this.data.filter(product => {
+      let productOrder: any = product[sortOrder];
+
+      if(sortOrder === 'price' || sortOrder ==='rating') {
+        return productOrder === Number(search);
+      } 
+      
+      if(sortOrder === 'inventoryStatus' || sortOrder === 'name')  {
+        return (productOrder.toUpperCase()).includes(search) === true;
+      }
+    });
   }
 }
