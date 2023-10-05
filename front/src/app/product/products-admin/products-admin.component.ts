0722cce1e70a6d0df2e36d4d64a6b01a3c3dd2ba
LucieTEni <lucie.touchard2019@campus-eni.fr>
2023-10-05 16:16:38 +0200

    create product list
    Add form, manage delete item and add new item

--- front/src/app/product/products-admin/products-admin.component.ts
+++ front/src/app/product/products-admin/products-admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Category, Product } from '../product.model';
+import { CrudItemOptions } from 'app/shared/utils/crud-item-options/crud-item-options.model';
+import { ControlType } from 'app/shared/utils/crud-item-options/control-type.model';
 
 @Component({
   selector: 'app-products-admin',
@@ -7,9 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsAdminComponent implements OnInit {
 
-  constructor() { }
+  public products: Product[] = [];
+  public productsCRUD: CrudItemOptions[] = [];
+  public productModel = Product;
+  constructor(private productService: ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.productService.getProducts();
+    this.getCRUDItemOptions();
+  }
+
+  getCRUDItemOptions(): void {
+    this.productsCRUD = [
+      {
+        label: 'Code',
+        key: 'code',
+        value: 'code',
+        columnOptions: {
+          default: true,
+        },
+        controlType: ControlType.INPUT,
+        type: 'number'
+      },
+      {
+        label: 'Name',
+        key: 'name',
+        value: 'name',
+        columnOptions: {
+          default: true,
+        },
+        controlType: ControlType.INPUT,
+        type: 'text',
+      },
+      {
+        label: 'Category',
+        key: 'category',
+        value: 'category',
+        columnOptions: {
+          default: false,
+        },
+        controlType: ControlType.SELECT,
+        options: [
+          {value: Category.ACCESSORIES, label: Category.ACCESSORIES},
+          {value: Category.CLOTHING, label: Category.CLOTHING},
+          {value: Category.ELECTRONICS, label: Category.ELECTRONICS},
+          {value: Category.FITNESS, label: Category.FITNESS},
+        ],
+      },
+      {
+        label: 'Description',
+        key: 'description',
+        value: 'description',
+        columnOptions: {
+          default: false,
+        },
+        controlType: ControlType.INPUT,
+        type: 'text'
+      },
+      {
+        label: 'Price',
+        key: 'price',
+        value: 'price',
+        columnOptions: {
+          default: false,
+        },
+        controlType: ControlType.INPUT,
+        type: 'number'
+      }
+    ];
+  }
+
+  saved(product: Product): void {
+    console.log(product);
+  }
+
+  deleted(ids: number[]): void {
+    this.products = this.products.filter(product => !ids.includes(product.id));
   }
 
 }
